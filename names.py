37f7d6eb8b9cfc054dae68c65dfd67a638b17ee1
hollyemblem <holly.emblem@gmail.com>
2019-02-21 20:57:03 +0000

    improved OO, still some work to go

--- names.py
+++ names.py
@@ -1,6 +1,6 @@
 # WIP!
 
-# Requires OO build and optimisation
+# Requires optimisation and tidy of OO methods.
 
 # Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
 
@@ -10,41 +10,49 @@
 # What is the total of all the name scores in the file?
 
 
-def file_open():
-    with open('names.txt') as f:
-        for line in f:
-            return line
+class FileManagement:
+    def __init__(self):
+        pass
+
+    def file_open(self, file_name):
+        file_list = []
+        with open(file_name) as f:
+            for line in f:
+                return line
+
+    def file_cleaner(self, file_version):
+        file_list = []
+        file_version = file_version.replace('"', '')
+        file_list = file_version.split(',')
+        file_list.sort()
+        return file_list
 
 
-def file_cleaner(file_version):
-    file_list = []
-    file_version = file_version.replace('"', '')
-    file_list = file_version.split(',')
-    file_list.sort()
-    return file_list
+class NamesManagement:
+
+    def __init__(self):
+        pass
+
+    def position_calculator(self, file_version):
+        for i in file_version:
+            return (int(file_version.index(i)) + 1)
+
+    def alphabet_calculator(self, file_version, index_value):
+        sum = 0
+        for i in file_version[index_value]:
+            print(i)
+            position_build = ord(i) - 64
+            sum = position_build + sum
+        return (sum * (index_value + 1))
 
 
-def position_calculator(file_version):
-    for i in file_version:
-        return (int(file_version.index(i)) + 1)
-
-
-def alphabet_calculator(file_version, index_value):
-    sum = 0
-    for i in file_version[index_value]:
-        print(i)
-        position_build = ord(i) - 64
-        sum = position_build + sum
-    return (sum * (index_value + 1))
-
-
-m = file_open()
-n = file_cleaner(m)
-position = position_calculator(n)
+m = FileManagement().file_open('names.txt')
+n = FileManagement().file_cleaner(m)
+position = NamesManagement().position_calculator(n)
 
 sum = 0
 i = 0
 while i < len(n):
-    sum = alphabet_calculator(n, i) + sum
+    sum = NamesManagement().alphabet_calculator(n, i) + sum
     i = i + 1
 print(sum)
